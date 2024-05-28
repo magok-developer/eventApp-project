e3375c9df1a18f78a0196dbe267881667840d3bb
Jiwon <hajw.study@gmail.com>
2024-05-28 22:14:32 +0900

    Textarea 컴포넌트 추가

---
+++ src/Components/Textarea/Textarea.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { color } from "../../styles/color";
+
+type Props = {
+  value: string;
+  style?: React.CSSProperties;
+  onChange: (e: any) => void;
+};
+
+const Textarea = ({ value, style, onChange }: Props) => {
+  return <TextareaStyle value={value} style={style} onChange={onChange} />;
+};
+
+export default Textarea;
+
+const TextareaStyle = styled.textarea`
+  width: 100%;
+  height: 300px;
+  border-radius: 6px;
+  padding: 10px;
+
+  background-color: ${color.skyBlue};
+  border: 1.5px solid ${color.deepBlue};
+  outline: none;
+
+  box-sizing: border-box;
+
+  resize: none;
+`;
