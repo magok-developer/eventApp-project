8e02c1a8f04561c0659fdaf18c9a131cece037df
Jiwon <hajw.study@gmail.com>
2024-05-30 01:55:11 +0900

    Header 컴포넌트 수정

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -24,35 +24,45 @@ const Header = ({
   onSearchChange,
   onSearch,
 }: Props) => {
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      onSearch();
+    }
+  };
   return (
     <Container>
-      <div className='wrap'>
-        <Calendar
-          selectedDate={selectedDate}
-          onDateChange={onDateChange}
-          showTimeSelect={false}
-          placeholderText='날짜 선택'
-          style={{ width: "130px" }}
-          dateFormat='yyyy-MM-dd'
-          isClearable={true}
-        />
-        <div className='button' onClick={handleSort}>
-          {sortAscending ? "최신 순 보기" : "오래된 순 보기"}
+      <h2 className='title'>Event Web Application</h2>
+      <HeaderWrap>
+        <div className='wrap'>
+          <Calendar
+            selectedDate={selectedDate}
+            onDateChange={onDateChange}
+            showTimeSelect={false}
+            placeholderText='날짜 선택'
+            style={{ width: "145px" }}
+            dateFormat='yyyy-MM-dd'
+            isClearable={true}
+          />
+          <div className='button' onClick={handleSort}>
+            {sortAscending ? "최신 순 보기" : "오래된 순 보기"}
+            <img src='images/icons/arrow/sort.svg' width={14} />
+          </div>
         </div>
-      </div>
-      <div className='wrap'>
-        <Input
-          value={searchQuery}
-          onChange={onSearchChange}
-          placeholder='검색'
-          onSearch={onSearch}
-          icon={true}
-        />
+        <div className='wrap'>
+          <Input
+            value={searchQuery}
+            onChange={onSearchChange}
+            placeholder='검색'
+            onSearch={onSearch}
+            icon={true}
+            onKeyDown={handleKeyDown}
+          />
 
-        <Link to={"/create"}>
-          <img src='images/icons/add/add.svg' alt='add' className='add' />
-        </Link>
-      </div>
+          <Link to={"/create"}>
+            <img src='images/icons/add/add.svg' alt='add' className='add' />
+          </Link>
+        </div>
+      </HeaderWrap>
     </Container>
   );
 };
@@ -60,6 +70,18 @@ const Header = ({
 export default Header;
 
 const Container = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  .title {
+    color: ${color.deepBlue};
+  }
+`;
+
+const HeaderWrap = styled.div`
   width: 100%;
   margin: 30px 0;
   padding: 0 30px;
@@ -68,7 +90,6 @@ const Container = styled.div`
   align-items: center;
   gap: 16px;
   box-sizing: border-box;
-
   .wrap {
     display: flex;
     flex-direction: row;
@@ -83,8 +104,12 @@ const Container = styled.div`
     border: 1.5px solid ${color.deepBlue};
     background-color: ${color.skyBlue};
     border-radius: 6px;
-    line-height: 8px;
     cursor: pointer;
+    font-size: 14px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    gap: 4px;
   }
 
   .text {
@@ -97,7 +122,6 @@ const Container = styled.div`
   }
 
   .add {
-    width: 30px;
     cursor: pointer;
   }
 `;
