ac70956cac00e45edb0f1cedcc0dce93ca7ed18c
Jiwon <hajw.study@gmail.com>
2024-05-30 01:55:03 +0900

    DataList 컴포넌트 수정

--- src/Components/DataList/DataList.tsx
+++ src/Components/DataList/DataList.tsx
@@ -1,23 +1,33 @@
+import styled from "styled-components";
 import { DataType } from "../../model/types";
 import DataItem from "../DataItem/DataItem";
 
 type Props = {
   data: DataType[];
-  onDelete(id: number | string): void;
+  onDelete(id: number): void;
 };
 
 const DataList = ({ data, onDelete }: Props) => {
   return (
-    <>
+    <Container>
       {data.map((item, index) => (
         <DataItem
-          key={`${item.id}` + `${index}`}
+          key={`${item.id}_${index}`}
           data={item}
-          onDelete={onDelete}
+          onDelete={() => {
+            onDelete(item.id);
+          }}
         />
       ))}
-    </>
+    </Container>
   );
 };
 
 export default DataList;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  margin-top: 20px;
+`;
